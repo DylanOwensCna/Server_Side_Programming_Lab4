e62377f240a6e1c7f3593147987a471b8a86a9cf
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-02-17 21:47:51 -0330

    Linking webpages with js files and adding css

--- Lab4Site/routes/contact-me.js
+++ Lab4Site/routes/contact-me.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'users' });
-});
+    res.render('contact-me', { title: 'Contact-me' });
+  });
+  
 
 module.exports = router;
